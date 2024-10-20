e43e0c64790331579baac2df260e8b072123b840
Yuunus <y.mah.ed@student.tue.nl>
2024-10-20 16:40:11 +0200

    Add necessary test for generate_noisy_image method

--- test/test_data_sets.py
+++ test/test_data_sets.py
@@ -1,4 +1,6 @@
 from data_sets import *
+import numpy as np
+import pytest
 
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))  # Mark the test root directory
@@ -9,4 +11,24 @@ TEST_IMAGE_TEST_DIR = os.path.join(TEST_DIR, "data_sets", "test_images")
 class TestDataSets:
     # You don't need to test generate_data_set() (that will take too much time to run)
 
-    pass  # Replace with your own tests
+    def test_generate_noisy_image(self):
+
+        """
+        Test if generate_noisy_image gives correct output
+        """
+    
+        #test if the image is generated has the correct shape
+        rank = random.choice(LABELS)
+        img = generate_noisy_image(rank, 0.7)
+
+        assert img.shape == (IMAGE_SIZE, IMAGE_SIZE)
+
+        #test for rank must be in LABELS for upper/lower case
+        assert pytest.raises(ValueError, generate_noisy_image, "ab", 0.5)
+        assert pytest.raises(ValueError, generate_noisy_image, "AB", 0.5)
+
+        #test for noise level must be between 0 and 1
+        assert pytest.raises(ValueError, generate_noisy_image, rank , -0.1) 
+        assert pytest.raises(ValueError, generate_noisy_image, rank , 1.1)
+
+
