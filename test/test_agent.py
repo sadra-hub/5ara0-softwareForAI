85bd432d4e607f7bae36d24b91f864d9976770f6
Yuunus <y.mah.ed@student.tue.nl>
2024-10-24 00:01:09 +0200

    Add test for agent for test_make_action method

--- test/test_agent.py
+++ test/test_agent.py
@@ -23,6 +23,34 @@ class TestPokerAgent:
         assert agent.model != None
         assert agent.model == mock_model
 
+    @patch('agent.load_model')
+    def test_make_action(self, mock_load_model):
+        """
+        Test whether the agent makes a correct action.
+        """
+        # Create a mock model and set it as the return value for load_model.
+        mock_model = Mock()
+        mock_load_model.return_value = mock_model
+
+        # Create a PokerAgent object, which will use the mocked `load_model`.
+        agent = PokerAgent()
+
+        # Create game state objects.
+        gamestate = ClientGameState("coordinator_1", "player_1", 100)
+        gameroundstate = ClientGameRoundState("coordinator_1", 1)
+        gameroundstate.set_card('K')
+
+        # Simulate a situation where the agent should choose the 'BET' action.
+        action = agent.make_action(gamestate, gameroundstate)
+        assert action == 'BET'
+
+        # Simulate an illegal action scenario.
+        gameroundstate.set_moves_history(['FOLD', 'BET'])
+        action = agent.make_action(gamestate, gameroundstate)
+        assert action == 'BET'
+
+
+
 
 
 
